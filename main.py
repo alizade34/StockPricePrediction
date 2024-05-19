c634936de42cd6b0137655ce7cc22cf3e6711ce0
alizade34 <emilalizade2001@gmail.com>
2024-05-19 16:35:26 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+import numpy as np
+import scipy.stats as stats
+from sklearn.preprocessing import StandardScaler
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout
+import gym
+from gym import spaces
+from stable_baselines3 import PPO
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import joblib
+import random
